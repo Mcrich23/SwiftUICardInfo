e1be55def73cedf978d169b6b9ba69c0a1f85254
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2023-03-19 11:51:34 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftUICardInfo
+
+A description of this package.
---
+++ Sources/SwiftUICardInfo/SwiftUICardInfo.swift
@@ -0,0 +1,6 @@
+public struct SwiftUICardInfo {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
